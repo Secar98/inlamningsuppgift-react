9b8dae5e25bc2f7184439ad897273c94834324fd
Sebastian Carlson <sebcar98@gmail.com>
2021-01-11 11:30:09 +0100

    add loginpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <div>
 
+
+
+
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
